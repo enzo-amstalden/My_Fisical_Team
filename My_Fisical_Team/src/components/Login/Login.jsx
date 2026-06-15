698eac3f83f435f5d514586830b43afdc9ecb76e
Enzo Amstalden <13enzomeida@gmail.com>
2026-06-15 20:14:41 -0300

    HTML da tela de login

---
+++ My_Fisical_Team/src/components/Login/Login.jsx
@@ -0,0 +1,43 @@
+import { FaUser, FaLock } from "react-icons/fa";
+import { useState } from "react";
+import './Login.css';
+
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Envio");
+  };
+
+  return (
+    <div className="container">
+        <form onSubmit={handleSubmit}>
+            <h1>Login</h1>
+            <div>
+                <input type="email" placeholder='E-mail'
+                onChange={(e) => setEmail(e.target.value)} />
+                <FaUser className="icon" />
+            </div>
+            <div>
+                <input type="password" placeholder='Senha' 
+                onChange={(e) => setPassword(e.target.value)} />
+                <FaLock className="icon" />
+            </div>
+            <div className="forgot-password">
+                <a href="#">Esqueceu sua senha?</a>
+            </div>
+
+
+            <button type="submit">Entrar</button>
+
+            <div className="signup-link">
+                <p>Não tem uma conta? <a href="#">Cadastre-se</a></p>
+            </div>
+        </form>
+    </div>
+  )
+}
+
+export default Login
